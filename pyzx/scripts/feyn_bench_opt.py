5f8ce06d1bf81de9ad9bdaae37cd1b05539aaf96
ru46yed <K.Staudacher@campus.lmu.de>
2022-02-24 12:54:27 +0000

    Evaluation + Optimierte Schaltkreise

--- pyzx/scripts/feyn_bench_opt.py
+++ pyzx/scripts/feyn_bench_opt.py
@@ -42,9 +42,10 @@ def evaluate_strategy(circuit, strategy, params = {}):
             return (None, None)
     t_d = time.time() - t_s
     if strategy == "pyzx":
-        c_ext = zx.Circuit.from_graph(g)
-    else:
-        c_ext = zx.extract_circuit(g.copy())
+        zx.simplify.clifford_simp(g)
+        # c_ext = zx.Circuit.from_graph(g)
+    # else:
+    c_ext = zx.extract_circuit(g.copy())
     c_ext = zx.optimize.basic_optimization(c_ext.to_basic_gates()).to_basic_gates().split_phase_gates()
     return (c_ext, t_d)
 
@@ -67,15 +68,15 @@ def evaluate_circuit(path, name, todd=False):
     stats.append(generate_stat_entry(c_opt, 'pyzx', time_opt))
     best_circuit = c_opt
 
-    for strategy in ['greedy_simp', 'random_simp', 'greedy_simp_neighbors', 'random_simp_neighbors']:
-        for cap in [1, -5, -20]:
-            print("evaluate strategy ",strategy)
-            params = {'cap': cap, 'max_v': False if cap == 1 else True}
-            c_opt, time_opt = evaluate_strategy(c_basic_opt, strategy, params)
-            stats.append(generate_stat_entry(c_opt, strategy, time_opt, params))
+#     for strategy in ['greedy_simp', 'random_simp', 'greedy_simp_neighbors', 'random_simp_neighbors']:
+#         for cap in [1, -5, -20]:
+#             print("evaluate strategy ",strategy)
+#             params = {'cap': cap, 'max_v': False if cap == 1 else True}
+#             c_opt, time_opt = evaluate_strategy(c_basic_opt, strategy, params)
+#             stats.append(generate_stat_entry(c_opt, strategy, time_opt, params))
 
-            if c_opt.twoqubitcount() < best_circuit.twoqubitcount():
-                best_circuit = c_opt
+#             if c_opt.twoqubitcount() < best_circuit.twoqubitcount():
+#                 best_circuit = c_opt
     
     return (best_circuit, stats)
 
@@ -87,7 +88,7 @@ def evaluate_folder(folder):
     list_of_files = sorted( list_of_files, key =  lambda x: os.stat(os.path.join(source_folder,x)).st_size)
     for file in list_of_files:
         print("evaluate circuit ",file, " at ",datetime.datetime.now())
-        for todd in [False, True]:
+        for todd in [False]:
             best_circuit, stats = evaluate_circuit(source_folder, file, todd)
 
             filename = file + '_t' if todd else file
